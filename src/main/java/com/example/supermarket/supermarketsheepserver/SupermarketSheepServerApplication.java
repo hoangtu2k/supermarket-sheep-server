7e9ea842cf1bce1465574c4cc99ebeb244cc75bc
Phung Hoang Tu <thocodeonline@gmail.com>
2025-04-23 22:49:41 +0700

    create server

---
+++ src/main/java/com/example/supermarket/supermarketsheepserver/SupermarketSheepServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.supermarket.supermarketsheepserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketSheepServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermarketSheepServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/supermarket/supermarketsheepserver/SupermarketSheepServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supermarket.supermarketsheepserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketSheepServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
